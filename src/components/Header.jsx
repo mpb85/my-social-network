b1fef8d78c0d643f888756c45dde29a8e2703e44
Your Name <you@example.com>
2020-01-30 02:48:34 +0200

    copy to file or repository

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://autodoktor.com.ua/wp-content/uploads/Logo/Total-logo-earth.png' />
+        </header>
+    );
+
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                <a href='#'>Profile</a>
+            </div>
+            <div>
+                <a href='#'>Masseges</a>
+            </div>
+            <div>
+                <a href='#'>News</a>
+            </div>
+            <div>
+                <a href='#'>Music</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
