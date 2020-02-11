f7dd0a41c098b44d48ca9342e6ebc86da9979ba1
Your Name <you@example.com>
2020-02-12 00:22:47 +0200

    struсturштп file and folders

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import s from './Profile.module.css'
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src='https://www.w3schools.com/howto/img_snow_wide.jpg' />
+            </div>
+            <div>
+                ava + my discription
+        </div>
+            <div>
+                my post
+        </div>
+            <div>
+                new post
+          <div>
+                    post 1
+          </div>
+                <div>
+                    post 2
+          </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Profile;
